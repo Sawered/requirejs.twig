94dabbb418f7bc9e967a1b5444e334608bcfd8cc
Sawered <sawered@gmail.com>
2014-06-18 09:41:44 +0400

    Rewrite optimization dump

--- requirejs.twig.js
+++ requirejs.twig.js
@@ -1,26 +1,27 @@
 /*
- * Requirejs plugin for loading twigjs (https://github.com/justjohn/twig.js) 
+ * Requirejs plugin for loading twigjs (https://github.com/justjohn/twig.js)
  * template functions for marionette.
- * 
- * This is practically a copy with slight modifications of Miller Medeiros 
+ *
+ * This is practically a copy with slight modifications of Miller Medeiros
  * https://github.com/millermedeiros/requirejs-plugins
- * 
+ *
  * Dependencies:
  * - requirejs/test https://github.com/requirejs/text
  * - twig.js https://github.com/justjohn/twig.js
- * 
+ *
  * @author Tim Joseph
- * @version 0.0.1 (2014/02/8) 
+ * @version 0.0.1 (2014/02/8)
  */
 define(['text', 'twig', 'underscore'], function(text, Twig, _) {
-    
+
     var buildMap = {};
-    
+
     // API
     return {
         load: function(name, req, onLoad, config) {
-            // This plugin only loads .twig files
-            name = name + '.twig';
+
+            // This plugin loads any files, just be shure they have
+            // correct markup
 
             text.get(req.toUrl(name), function(data) {
                 if (config.isBuild) {
@@ -38,13 +39,27 @@ define(['text', 'twig', 'underscore'], function(text, Twig, _) {
                     onLoad(render);
                 }
             });
-            
+
         },
-        
+
         write: function(pluginName, moduleName, write) {
-            if (moduleName in buildMap) {
+
+            if (buildMap.hasOwnProperty(moduleName)) {
+
+                var name = pluginName + '!' + moduleName;
                 var content = buildMap[moduleName];
-                write('define("' + pluginName + '!' + moduleName + '", function(){ return ' + content + ';});\n');
+                var template = Twig.twig({
+                    data: content,
+                    id:name
+                });
+
+                var compiled = template.compile({
+                    twig:'twig',
+                //    module:'amd', //we not use twigjs amd export, as we need add dependencies
+                });
+
+               var amd_define =  'define("'+name+'",["twig","underscore"], function (Twig,_) {\n\tvar twig, template;\ntwig = Twig.twig;\ntemplate = ' + compiled + ';\n\t var render = _.bind(template.render, template);\n\t return render;\n});';
+               write(amd_define);
             }
         }
 
